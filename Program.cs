9fadf01c3d321d1f8111eebc8bdeda16904fe4bc
Melih Ercan <melihercan@gmail.com>
2020-05-21 22:11:29 +0300

    Started with rpn program.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace rpn
 {
+
+
     class Program
     {
+        static Dictionary<string, Action> Operators = new Dictionary<string, Action> 
+        {
+            ["+"] = Add,
+        };
+
+        private static void Add()
+        {
+            Console.WriteLine("add is called");
+        }
+
+        const string prompt = "> ";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(args.Length == 0)
+            {
+                // Interactive.
+                CommandLoop();
+            }
+            else
+            {
+                // Evaluate one line expression and exit.
+
+            }
+
+
         }
+
+        static void CommandLoop()
+        {
+            bool isExit = false;
+            while (!isExit)
+            {
+                DisplayPrompt();
+                var readLine = Console.ReadLine();
+                if(readLine == null)
+                {
+                    // CTRL-C.
+                    isExit = true;
+                }
+
+                ParseInput(readLine);
+
+                Operators["+"].Invoke();
+
+            }
+        }
+
+        private static void ParseInput(string readLine)
+        {
+            var tokens = readLine.Split(" ").Where(_ => _ != string.Empty);
+            foreach (var token in tokens)
+                Console.WriteLine(token);
+        }
+
+        private static void DisplayPrompt()
+        {
+            Console.Write(prompt);
+        }
+
     }
 }
